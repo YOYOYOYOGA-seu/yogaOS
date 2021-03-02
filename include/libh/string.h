0bc789a24f0d78983264558e87e9da26d7b449cb
YOYOYOYOGA-seu <2067634068@qq.com>
2021-03-02 18:44:40 +0800

    加入新的系统调用sys_free sys_malloc sys_panic

--- include/libh/string.h
+++ include/libh/string.h
@@ -1,7 +1,7 @@
 /*
  * @Author Shi Zhangkun
  * @Date 2020-02-25 02:50:21
- * @LastEditTime 2020-04-18 02:01:31
+ * @LastEditTime 2020-12-30 00:16:32
  * @LastEditors Shi Zhangkun
  * @Description none
  * @FilePath /project/include/libh/string.h
@@ -11,8 +11,8 @@
 #include "yogaOS/types.h"
 
 void * memcpy(void * dest, const void *src, int n);
-
 void * memset(void *s, int ch, ssize_t n);
 char *strcpy(char* dest, const char *src);
 int strcmp(const char* str1, const char *str2);
+int memcmp(const void *str1, const void *str2, size_t n);
 #endif
